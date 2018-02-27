c1340a59497b18d989d218bf779032bc65bc9d7c
Michel Gomes da Silva <michel@uconex.com.br>
2018-02-26 23:23:02 -0300

    managing server errors

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
-console.log('API rodando na porta: ' + port);
+server.on('error', onError);
+server.on('listening', onListening);
+console.log('API rodando na porta ' + port);
 
 function normalizePort(val) {
     const port = parseInt(val, 10);
@@ -35,4 +37,35 @@ function normalizePort(val) {
     }
 
     return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
 }
